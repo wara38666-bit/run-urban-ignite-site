4338aff43b60559c1c4ef7d0d54c98a7505cd4c8
wara38666-bit <wara38666@gmail.com>
2026-03-11 21:01:29 +0700

    update all

--- script.js
+++ script.js
@@ -69,4 +69,4 @@ if ("IntersectionObserver" in window) {
   revealItems.forEach((item) => item.classList.add("in-view"));
 }
 
-console.log("RUN Urban Ignite new cinematic theme loaded");
+console.log("RUN Urban Ignite hype sports urban theme loaded");
